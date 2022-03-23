140ce92e07948c17e8d932279663b4fa66cf4c72
Ru37n3M <Ru37n3M@users.noreply.github.com>
2022-03-23 12:50:56 -0300

    5th commit

--- Criterio_A+Opinion_participante.R
+++ Criterio_A+Opinion_participante.R
@@ -1,6 +1,6 @@
 
 #Criterio A + Oi
-
+#funcion explicada en "function_helper"
 vl_list_A_Oi<- voting_loop_Oi(rnorm(100,0,1),5)
 vl_list_A_Oi
 
--- logica_sim.R
+++ logica_sim.R
@@ -1,62 +1,6 @@
 
+#funcion base
+#explicado en el archivo function helper
+vl_base<- voting_loop_base(df_I_n_list(rnorm(100,0,1)),5)
 
-n <- rnorm(100, 0, 1) #cantidad de participantes
-
-I <- rnorm(30, 0, 1) #pool de ideas
-
-participantes <- n #copia de n para ser modificada en runtime env de deal()
-PARTICIPANTES <- n #copia para restaurar vector "participantes" luego de que fue modificada por deal ()
-                   #se usa en la funcion reset()
-
-deal <- function() {
-  
-  df_k <- data.frame (
-    "valor" = c(I),
-    "visualizaciones" = 0,
-    "V_pos" = 0,
-    "V_neg" = 0
-  )               #dataframe para cargar datos sobre visualizaciones y votos de ideas
-  
-  repeat { #loop de repeticion
-    
-    k <- sample( I, 5 ) #cantidad de I que el participante puede ver
-    
-    par <- participantes[1] #orden de entrada secuencial de participantes (1 en 1)
-    
-    assign("participantes", participantes[-1], envir = globalenv()) #se modifica el vector participantes creado a partir
-    #de n, se quita al primer participante del vector para 
-    #permitir que entre otro participante
-    
-    df_k$visualizaciones[which(I%in%k)] <- df_k$visualizaciones[which(I%in%k)] + 1 #se suma 1 a la dimension "visualizacion" del df a las ideas I presentes 
-    # en k
-    
-    dist <- ( par - k )  #Se calcula la distancia de K con respecto al valor del participante
-                          #se suma 1 cada valor del vector para evitar la aparicion de valores negativos y usar
-                          #min como evaluador logico 
-                          #agente ve k. evaluacion logica para ver cual k tiene menor distancia con On
-    
-    v_pos <-  which( I%in%k[which.closest(dist, 0)] ) #se toma el valor más cercano a 0 
-    #en relacion a la distancia con el valor participante
-    v_neg <- which ( I%in%k[which.max(abs(dist))] ) #se toma el valor maximo 
-    #en relacion a la distancia con el valor participante
-    #se usa abs para evitar confusion con numeros negativos
-    
-    df_k$V_pos[v_pos] <- df_k$V_pos[v_pos] + 1 #se suma 1 punto a la idea que se corresponde con el valor minimo
-    df_k$V_neg[v_neg] <- df_k$V_neg[v_neg] + 1 #se suma 1 punto a la idea que se corresponde con el valor maximo
-    
-    if ( is.na(par) ) #el loop va a repetir el codigo de arriba hasta que la condicion is.na sea TRUE
-                    #va a modificar el dataframe a medida que se descartan participantes
-      
-      break
-    
-  }
-  
-  df_k #retorna dataframe modificado
-}
-
-reset <- function() {
-  assign( "participantes", PARTICIPANTES[], envir = globalenv() )
-}
-
-reset () #se resetea el vector participantes 
-deal() #retorna dataframe con visualizaciones y votos del total de participantes
+vl_base
