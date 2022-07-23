301b2974fec61d5682fce449a38b828bfc10a0f3
Ru37n3M <Ru37n3M@users.noreply.github.com>
2022-07-23 11:51:40 -0300

    Actualizado MVD03

--- MVD03.R
+++ MVD03.R
@@ -1,16 +1,11 @@
-library(MASS)
-mvdlist <- list("1" = data.frame(mvrnorm(n = 200, c(2, 2, 2),diag(1,3,3))),
-                "2" = data.frame(mvrnorm(n = 200, c(-2, -2, -2),diag(1,3,3))),
-                "3" = data.frame(mvrnorm(n = 600, c(0, 0, 0),diag(1,3,3))))
+
+mixingfun <- function(dislist){
+  
+  dislist
+  
+}
 
 
-mvdf <- do.call("rbind", mvdlist)
-
-mvdf
-
-library(MVN)
-mvn(mvdf, mvnTest = "energy", univariatePlot = "scatter")
-
-
-
-
+mixingfun(list("n" = c(200,300,100), # n de cada distribucion
+               "means" = list(c(2,2,2),c(1,1,1),c(0,0,0)), # lista con vectores de medias para cada dist
+               "cov_mat" = list(diag(1,3,3),diag(1,3,3),diag(1,3,3)))) # lista con matrices de covarianza
