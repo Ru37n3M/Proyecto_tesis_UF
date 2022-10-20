1ed3101d4f7c79f53113af1678845c315ce5d86e
Aler-u <51932853+Aler-u@users.noreply.github.com>
2022-10-20 14:19:58 -0300

    Eliminado objeto dist
    Se elimino un llamado a la funcion mixingfun que creaba el objeto dist.

--- function_helper.R
+++ function_helper.R
@@ -29,13 +29,6 @@ mixingfun <- function(dislist, beta, total_votos){
 }
 
 
-dist <- mixingfun(list("n" = c(200,300,100), # n de cada distribucion
-                       "means" = list(c(2,2,2),c(1,1,1),c(0,0,0)), # lista con vectores de medias para cada dist
-                       "cov_mat" = list(diag(1,3,3),diag(1,3,3),diag(1,3,3))), # lista con matrices de covarianza
-                  rbeta(600,4,2), #Distribucion beta, n == sum(list$n)
-                  2) #Size de distribucion binomial 
-
-
 #####
 shuffle_dist <- function(distr){
   
